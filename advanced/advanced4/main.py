9f9a10607ecbffb38ca7385e5229370eaf74a3d6
yooodleee <accia25@naver.com>
2025-09-03 19:50:27 +0900

    advanced

---
+++ advanced/advanced4/main.py
@@ -0,0 +1,13 @@
+# Python v2.7
+# input()
+intInput = input("integer input : ")
+print(type(intInput))
+
+stringInput = input("String input : ")
+print(type(stringInput))
+
+# 결과
+# Integer input : 10
+# <type 'int'>
+# String input : "python"
+# <type 'str'>
---
+++ advanced/advanced5/main.py
@@ -0,0 +1,10 @@
+# Python 3.7
+# sys.stdin.readline()
+import sys
+
+a = sys.stdin.readline().strip()
+print("input : ", a)
+
+# 결과
+# 1 2 3 4 5
+# input: 1 2 3 4 5 
