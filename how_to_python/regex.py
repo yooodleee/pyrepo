8d0f155125ef858f1dce57bf565a13e0732c63ef
yooodleee <accia25@naver.com>
2025-09-15 09:52:01 +0900

    regex match group

--- how_to_python/regex.py
+++ how_to_python/regex.py
@@ -216,3 +216,46 @@ re.sub(r"\D", "-", '123,456_789')
 
 
 # %%
+text_data = """101, Homework; Complete physics and math
+some random nonsense
+102, Laundry; Wash all the clothes today
+54, random; record
+103, Museum; All about Egypt
+1234, random; record
+Another random record"""
+
+
+regex = re.compile(r"(\d{3}), (\w+); (.+)")
+for line in text_data.split("\n"): 
+    match = regex.match(line)
+    if match:
+        print(f"{'Matched:':<12}{match.group()}")
+    else:
+        print(f"{'No Match:':<12}{line}")
+
+# # output:
+# Matched:    101, Homework; Complete physics and math
+# No Match:   some random nonsense
+# Matched:    102, Laundry; Wash all the clothes today
+# No Match:   54, random; record
+# Matched:    103, Museum; All about Egypt
+# No Match:   1234, random; record
+# No Match:   Another random record
+
+
+# %%
+regex = re.compile(r"(\d{3}), (\w+); (.+)")
+tasks = []
+
+for line in text_data.split("\n"):
+    match = regex.match(line)
+    if match:
+        task = (match.group(1), match.group(2), match.group(3))
+
+        tasks.append(task)
+
+print(tasks)
+# output: [('101', 'Homework', 'Complete physics and math'), ('102', 'Laundry', 'Wash all the clothes today'), ('103', 'Museum', 'All about Egypt')]
+
+
+# %%
