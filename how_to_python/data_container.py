2b748060acb9c7cfb370d5e2d60fb144ffd18788
yooodleee <accia25@naver.com>
2025-09-16 17:13:02 +0900

    sort list

--- how_to_python/data_container.py
+++ how_to_python/data_container.py
@@ -39,3 +39,39 @@ integers_tuple[0] = 'zero'
 
 
 # %%
+numbers = ([1, 2], [1, 2])
+
+numbers[0].append(3)
+
+print(numbers)
+# output: ([1, 2, 3], [1, 2])
+
+
+# %%
+tasks = [
+    {'title': 'Laundry', 'desc': 'Wash clothes', 'urgency': 3},
+    {'title': 'Homework', 'desc': 'Physics', 'urgency': 5},
+    {'title': 'Museum', 'desc': 'Egyptian things', 'urgency': 2}
+]
+
+tasks.sort()
+# TypeError: '<' not supported between instances of 'dict' and 'dict'
+
+
+# %%
+numbers = [12, 4, 1, 3, 7, 5, 9, 8]
+numbers.sort() 
+print(numbers)
+# output: [1, 3, 4, 5, 7, 8, 9, 12]
+
+names = ['Danny', 'Aaron', 'Zack', 'Jennifer', 'Mike', 'David']
+names.sort(reverse=True)
+print(names)
+# output: ['Zack', 'Mike', 'Jennifer', 'David', 'Danny', 'Aaron']
+
+mixed = [3, 1, 2, 'John', ['c', 'd'], ['a', 'b']]
+mixed.sort()
+# output: '<' not supported beween instances of 'str' and 'int'
+
+
+# %%
