1ad03b100cede8ea9f4908c82d1c4d1f5155e978
yooodleee <accia25@naver.com>
2025-09-18 13:57:00 +0900

    slicing

---
+++ how_to_python/sequence_data.py
@@ -0,0 +1,24 @@
+#############################
+## Sequence Data
+#############################
+
+
+# %%
+text = "Hello, World!"
+
+fruits = ["apple", "orange", "banana", "strawberry"]
+
+vowels = ("a", "b", "i", "o", "u")
+
+assert fruits[1:3] == ["orange", "banana"]
+
+
+# %%
+assert fruits[:3] == ["apple", "orange", "banana"]
+
+assert fruits[1:] == ["orange", "banana", "strawberry"]
+
+assert fruits[:] == ["apple", "orange", "banana", "strawberry"]
+
+
+# %%
