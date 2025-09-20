a8189c39f5949ed3981f806912eff51a5de5b001
yooodleee <accia25@naver.com>
2025-09-20 16:55:01 +0900

    raw string

--- free/regex.py
+++ free/regex.py
@@ -51,3 +51,55 @@ print(parts)
 
 
 # %%
+task_pattern = re.compile("\\\\task")   # \\, \\task 
+texts = ["\task", "\\task", "\\\task", "\\\\task"]
+
+for text in texts:
+    print(f"Match {text!r}: {task_pattern.match(text)}")
+
+# # output: 
+# Match '\task': None
+# Match '\\task': <re.Match object; span=(0, 5), match='\\task'>
+# Match '\\\task': None
+# Match '\\\\task': None
+
+
+# %%
+task_pattern = re.compile("\\\\pattern")    # \\, \\pattern 
+texts = ["\pattern", "\\pattern", "\\\pattern", "\\\\pattern"]
+
+for text in texts:
+    print(f"Match {text!r}: {task_pattern.match(text)}")
+
+# output: 
+
+
+# %%
+task_pattern_r = re.compile(r"\\task")      # raw string: \\task 
+texts = ["\task", "\\task", "\\\task", "\\\\task"]
+
+for text in texts:
+    print(f"Match {text!r}: {task_pattern_r.match(text)}")
+
+# # output: 
+# Match '\task': None
+# Match '\\task': <re.Match object; span=(0, 5), match='\\task'>
+# Match '\\\task': None
+# Match '\\\\task': None
+
+
+# %%
+task_pattern_r = re.compile(r"\\pattern")
+texts = ["\pattern", "\\pattern", "\\\pattern", "\\\\pattern"]
+
+for text in texts:
+    print(f"Match {text!r}: {task_pattern_r.match(text)}")
+
+# # output: 
+# Match '\\pattern': <re.Match object; span=(0, 8), match='\\pattern'>
+# Match '\\pattern': <re.Match object; span=(0, 8), match='\\pattern'>
+# Match '\\\\pattern': None
+# Match '\\\\pattern': None
+
+
+# %%
