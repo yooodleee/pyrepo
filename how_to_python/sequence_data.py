f754223611a0f40d06def960fe2cf2f1664d8b4e
yooodleee <accia25@naver.com>
2025-09-22 13:53:10 +0900

    range vs. slice

--- how_to_python/sequence_data.py
+++ how_to_python/sequence_data.py
@@ -22,3 +22,60 @@ assert fruits[:] == ["apple", "orange", "banana", "strawberry"]
 
 
 # %%
+fruits[5]
+# output: IndexError: list index out of range 
+
+
+# %%
+# bad case
+numbers = [0, 1, 2, 3, 4, 5]
+
+numbers[:20]
+# output: [0, 1, 2, 3, 4, 5]
+
+numbers[-1000:2]
+# output: [0, 1]
+
+
+# %%
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+assert numbers[2:5:2] == [3, 5]
+
+assert numbers[::3] == [1, 4, 7]
+
+assert numbers[::-1] == [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+# %%
+slice_obj = slice(1, 10, 2)
+range_obj = range(1, 10, 2)
+
+slice_obj.start, slice_obj.stop, slice_obj.step
+# output: (1, 10, 2)
+
+range_obj.start, range_obj.stop, range_obj.step
+# output: (1, 10, 2)
+
+
+# %%
+list(range(10))
+# output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+list(slice(10))
+# output: TypeError: 'slice' object is not iterable
+
+
+# %%
+numbers = list(range(10))
+
+odd_slice = slice(1, 10, 2)
+numbers[odd_slice]
+# output: [1, 3, 5, 7, 9]
+
+odd_range = range(1, 10, 2)
+numbers[odd_range]
+# output: TypeError: list indices must be integers or slices, not range
+
+
+# %%
