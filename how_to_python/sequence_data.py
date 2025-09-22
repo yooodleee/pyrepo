a518ba6aa9184933609aa4d94ef4f8c2627a6041
yooodleee <accia25@naver.com>
2025-09-22 15:43:53 +0900

    index

--- how_to_python/sequence_data.py
+++ how_to_python/sequence_data.py
@@ -196,3 +196,67 @@ def process_item_check_first(item):
 
 
 # %%
+# some pseudo code
+# def find_string(substr):
+#     str_index = the_str.find(substr)    # find -> return -1
+#     if str_index >= 0:
+#         # do something with the str_index
+#     else:
+#         # do something when the substr isn't preseont
+
+
+# %%
+class Task:
+    def __init__(self, title, urgency):
+        self.title = title
+        self.urgency = urgency
+
+tasks = [
+    Task("Laundry", 3),
+    Task("Museum", 4),
+    Task("Homework", 5),
+    Task("Ticket", 2)
+]
+
+needed_urgency = 5
+needed_task_index = None
+
+for task_i in range(len(tasks)):
+    task = tasks[task_i]
+    if task.urgency == needed_urgency:
+        needed_task_index = task_i
+        break
+
+print(f"Task Index: {needed_task_index}")
+# output: Task Index: 2
+
+
+# %%
+tasks.index(Task("Homework", 5))
+# output: ValueError: <__main__.Task object at 0x000002817975E790> is not in list
+
+
+# %%
+id(Task("Homework", 5))
+# output: 2755092089776
+
+id(tasks)
+# output: 2755113601472
+
+id(needed_task_index)
+# output: 2755010783568
+
+id(tasks.index(Task("Homework", 5)))
+# output: ValueError: <__main__.Task object at 0x00000281784F0AF0> is not in list
+
+id(tasks.index(Task("Laundry", 3)))
+# output: ValueError: <__main__.Task object at 0x000002817980E220> is not in list
+
+id(tasks.index(Task("Museum", 4)))
+# output: ValueError: <__main__.Task object at 0x0000028179894640> is not in list
+
+id(tasks.index(Task("Ticket", 2)))
+# output: ValueError: <__main__.Task object at 0x0000028179776A30> is not in list
+
+
+# %%
