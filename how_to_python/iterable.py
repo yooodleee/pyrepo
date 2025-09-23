214eed706ec89aede5508c087f62240b977d02dd
yooodleee <accia25@naver.com>
2025-09-23 11:44:23 +0900

    isIterable

--- how_to_python/iterable.py
+++ how_to_python/iterable.py
@@ -53,3 +53,55 @@ for task in tasks:  # auto except StopIteration
 
 
 # %%
+iter(5)
+# output: TypeError: 'int' object is not iterable
+
+iter([1, 2, 3])
+# output: <list_iterator at 0x25926889160>
+
+
+# %%
+def isIterable(obj):
+    try:
+        _ = iter(obj)
+    except TypeError:
+        print(type(obj), "is not an iterable")
+    else:
+        print(type(obj), "is an iterable")
+
+
+isIterable(5)
+# output: <class 'int'> is not an iterable
+
+isIterable([1, 2, 3])
+# output: <class 'list'> is an iterable
+
+isIterable("Hello")
+# output: <class 'str'> is an iterable
+
+isIterable((1, 2, 3))
+# output: <class 'tuple'> is an iterable
+
+isIterable({"one": 1, "two": 2})
+# output: <class 'dict'> is an iterable
+
+isIterable(range(3)) 
+# output: <class 'range'> is an iterable
+
+isIterable(map(int, ["1", "2"]))
+# output: <class 'map'> is an iterable
+
+isIterable(zip([1, 2], [3, 4]))
+# output: <class 'zip'> is an iterable
+
+isIterable(filter(bool, [1, None]))
+# output: <class 'filter'> is an iterable
+
+isIterable(enumerate([1, 2, 3]))
+# output: <class 'enumerate'> is an iterable
+
+isIterable(reversed("Hello")) 
+# output: <class 'reversed'> is an iterable
+
+
+# %%
