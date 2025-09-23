a015d4af34ae69d93a6d648fe1dd0e123ba7fa0c
yooodleee <accia25@naver.com>
2025-09-23 10:32:31 +0900

    list vs. deque

--- how_to_python/sequence_data.py
+++ how_to_python/sequence_data.py
@@ -356,3 +356,65 @@ task_id, *_, task_status = task
 
 
 # %%
+clients = list()
+
+def check_in(client):
+    clients.append(client)
+    print(f"in: New client {client} joined the queue.")
+
+
+def connect_to_associate(associate):
+    if clients:
+        client_to_connect = clients.pop(0)  # time compleixty -> list's pop() -> O(n) -> bad case
+        print(f"out: Remove {client_to_connect}, connecting to {associate}.")
+    
+    else:
+        print("No more clients are waiting.")
+
+
+
+# %%
+from collections import deque
+from timeit import timeit
+
+def time_fifo_testing(n):
+    integer_l = list(range(n))
+    integer_d = deque(range(n))
+
+    t_l = timeit(lambda : integer_l.pop(0), number=n)
+    t_d = timeit(lambda : integer_d.popleft(), number=n)
+
+    return f"{n: >9} list: {t_l:.6e} | deque: {t_d:.6e}"
+
+
+numbers = (100, 1000, 10000, 100000)
+for number in numbers:
+    print(time_fifo_testing(number))
+
+# # output: 
+# 100 list: 1.050000e-05 | deque: 4.200000e-06
+#      1000 list: 8.070000e-05 | deque: 4.110000e-05
+#     10000 list: 7.743850e-02 | deque: 3.982000e-04
+#    100000 list: 7.555413e+00 | deque: 3.895600e-03
+
+
+# %%
+from collections import deque
+
+clients = deque()
+
+def check_in(client):
+    clients.append(client)
+    print(f"in: New client {client} joined the queue.")
+
+
+def connect_to_associate(associate):
+    if clients:
+        client_to_connect = clients.popleft()
+        print(f"out: Removing {client_to_connect}, connecting to {associate}.")
+    
+    else:
+        print(f"No more clients are waiting.")
+
+
+# %%
