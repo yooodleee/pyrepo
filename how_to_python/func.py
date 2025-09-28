6f1f85bee09af54c2d8ade8361fc7e48da2cccbb
yooodleee <accia25@naver.com>
2025-09-28 16:20:02 +0900

    mutable argument

--- how_to_python/func.py
+++ how_to_python/func.py
@@ -60,3 +60,68 @@ def complete_task(task, note=""):
 
 
 # %%
+# eval func when definition function. -> generate mutable default instance.
+# generate list instance with eval function. -> side effect.
+def complete_task(task, grouped_tasks=[]):
+    task.status = "completed"
+    grouped_tasks.append(task.title)
+    
+    return grouped_tasks
+
+
+# %%
+task0 = Task("Homework", "Physics and math", 5)
+task1 = Task("Fishing", "Fishing at the late", 3)
+
+work_tasks = complete_task(task0)
+play_tasks = complete_task(task1)
+
+print("Work Tasks:", work_tasks)
+print("Play Tasks:", play_tasks)
+# # output: 
+# Work Tasks: ['Homework', 'Fishing']
+# Play Tasks: ['Homework', 'Fishing']
+
+
+# %%
+assert work_tasks == play_tasks
+
+assert work_tasks is play_tasks
+
+
+# %%
+def append_task(task, tasks=[]):
+    tasks.append(task)
+    print(f"Tasks: {tasks}; id: {id(tasks)}") # id(): return identity of object.
+
+
+append_task.__defaults__    # search default object.
+# output: ([],)
+
+id(append_task.__defaults__[0])
+# output: 2382810160960
+
+append_task("Homework")
+# output: Tasks: ['Homework']; id: 238210160960
+
+append_task("Laundry")
+# output: Tasks: ['Homework', 'Laundry']; id: 2382809796800
+
+append_task.__defaults__
+# output: (['Homework', 'Laundry'])
+
+
+# %%
+def complete_task(task, grouped_tasks=None):    # set mutable argument to None.
+    task.status = "completed"
+    if grouped_tasks is None:
+        grouped_tasks = []
+    
+    grouped_tasks.append(task.title)
+    return grouped_tasks
+
+complete_task.__defaults__
+# output: (None,)
+
+
+# %%
