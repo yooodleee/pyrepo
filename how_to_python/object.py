aa5927085a8180847a5a68f58f4da1d3e3299454
yooodleee <accia25@naver.com>
2025-10-14 15:50:43 +0900

    type vs. isinstance

--- how_to_python/object.py
+++ how_to_python/object.py
@@ -23,3 +23,65 @@ def filter_tasks(tasks, by_urgency):
 
 
 # %%
+# modified case: type
+def filter_tasks(tasks, by_urgency):
+    if type(by_urgency) is list:
+        filtered = [x for x in tasks if x.urgency in by_urgency]
+    else:
+        filtered = [x for x in tasks if x.urgency == by_urgency]
+    return filtered
+
+# %%
+# modified case: isinstance
+def filter_tasks(tasks, by_urgency):
+    if isinstance(by_urgency, list):
+        filtered = [x for x in tasks if x.urgency in by_urgency]
+    else:
+        filtered = [x for x in tasks if x.urgency == by_urgency]
+    return filtered
+
+
+# %%
+# comparison type with isinstance
+class User:     # parent class
+    pass
+
+class Supervisor(User): # child class
+    pass
+
+supervisor = Supervisor()
+
+comparisons = [
+    type(supervisor) is User,
+    type(supervisor) is Supervisor,
+    isinstance(supervisor, User),
+    isinstance(supervisor, Supervisor) 
+]
+
+comparisons
+# output: [False, True, True, True]
+
+
+# %%
+# bad case: NonCollection(abc module)
+def filter_tasks(tasks, by_urgency):
+    if isinstance(by_urgency, (list, tuple)):
+        filtered = [x for x in tasks if x.urgency in by_urgency]
+    else:
+        filtered = [x for x in tasks if x.urgency == by_urgency]
+    return filtered
+
+
+# %%
+# better case: Collection(abc module)
+from collections.abc import Collection
+
+def filter_tasks(tasks, by_urgency):
+    if isinstance(by_urgency, Collection):
+        filtered = [x for x in tasks if x.urgency in by_urgency]
+    else:
+        filtered = [x for x in tasks if x.urgency == by_urgency]
+    return filtered
+
+
+# %%
