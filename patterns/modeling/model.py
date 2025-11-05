179680872136f9fc1d04aa57031f28a62bef13dc
yooodleee <accia25@naver.com>
2025-11-05 09:15:06 +0900

    domain service

--- patterns/modeling/model.py
+++ patterns/modeling/model.py
@@ -4,6 +4,9 @@ from datetime import date
 from typing import Optional, List, Set
 
 
+class OutOfStock(Exception):
+    pass
+
 @dataclass(frozen=True)    
 class OrderLine:
     orderid: str
@@ -30,6 +33,14 @@ class Batch:
     def __hash__(self):
         return hash(self.reference)
     
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        
+        return self.eta > other.eta
+    
     def allocate(self, line: OrderLine):
         if self.can_allocate(line):
             self._allocations.add(line)
@@ -48,3 +59,15 @@ class Batch:
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
+    
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(
+        b for b in sorted(batches) if b.can_allocate(line)
+        )
+    except StopIteration:
+        raise OutOfStock(f'Out of stock for sku {line.sku}')
+    
+    batch.allocate(line)
+    return batch.reference
