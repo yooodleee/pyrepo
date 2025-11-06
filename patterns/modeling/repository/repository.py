82ef86c8d687aa2289d6dc27e057b775568e1b78
yooodleee <accia25@naver.com>
2025-11-06 19:44:51 +0900

    fake repository

--- patterns/modeling/repository/repository.py
+++ patterns/modeling/repository/repository.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
-from patterns.modeling.model.model import Batch
+from patterns.modeling.entity.model import Batch
 
 
+# Port(interface)
 class AbstractRepository(ABC):
 
     @abstractmethod
@@ -12,6 +13,7 @@ class AbstractRepository(ABC):
     def get(self, reference) -> Batch:
         raise NotImplementedError
 
+# Adapter(implementation)
 class SqlAlchemyRepository(AbstractRepository):
     def __init__(self, session):
         self.session = session
@@ -21,3 +23,20 @@ class SqlAlchemyRepository(AbstractRepository):
     
     def get(self, reference):
         return self.session.query(Batch).filter_by(reference=reference)
+
+# Adapter(implementation)
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches):
+        self._batches = set(batches)
+    
+    def add(self, batch):
+        self._batches.add(batch)
+    
+    def get(self, reference):
+        return next(
+            b for b in self._batches
+            if b.reference == reference
+        )
+    
+    def list(self):
+        return list(self._batches)
