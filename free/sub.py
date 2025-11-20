3990931e4146b66d54657e522d9b98639840e64d
yooodleee <accia25@naver.com>
2025-11-20 11:04:13 +0900

    ellipsis

--- free/sub.py
+++ free/sub.py
@@ -712,3 +712,53 @@ print(temp())
 
 
 # %%
+# Ellipsis
+def some_func():
+     ...
+    
+def another_func():
+     ...
+
+
+# %%
+# calc nth old num
+def nth_odd(n):
+	if isinstance(n, int): return 2 * n - 1
+	else: return None
+
+# calc the original n of nth odd num
+def original_num(m=...):
+	if m is ...:
+		print('This func needs some input')
+	elif m is None:
+		print('Non integer input provided to nth_odd() func')
+	elif isinstance(m, int):
+		if m % 2:
+			print(f'{m} is {int(((m + 1) / 2))}th odd number')
+		else:
+			print(f'{m} is not an odd number')
+
+original_num()
+
+a = nth_odd(n='some string')
+original_num(a)
+
+b = nth_odd(5)
+original_num(b)
+
+original_num(16)
+
+
+# %%
+import numpy as np
+
+a = np.arange(16).reshape(2, 2, 2, 2)
+
+print(a[..., 0].flatten())
+# [ 0  2  4  6  8 10 12 14]
+
+print(a[:, :, :, 0].flatten())
+# [ 0  2  4  6  8 10 12 14]
+
+
+# %%
