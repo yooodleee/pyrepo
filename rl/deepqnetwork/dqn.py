4559c8b3de9cddae30d6fa93e1491a9ac2925f80
yooodleee <accia25@naver.com>
2025-12-06 00:00:46 +0900

    minor fixes, including keras

--- rl/deepqnetwork/dqn.py
+++ rl/deepqnetwork/dqn.py
@@ -4,11 +4,12 @@ import os
 import cv2
 import gym
 import tensorflow as tf
-from tensorflow.python import keras
+import keras                # from tensorflow.python import keras
 import tf_slim as slim
 from tensorpack import *
 
 from atar_wrapper import FireRestEnv, FrameStack, LimitLength, MapState
+from common import Evaluator, eval_model_multithread, play_n_episodes
 from common import Model as DQNModel
 from expreplay import ExReplay
 
@@ -62,4 +63,104 @@ class Model(DQNModel):
             )
         image = image / 255.0
         
-        with slim.arg_scope(keras.layers.Conv2D)
+        with slim.arg_scope(keras.layers.Conv2D, 
+                            activation=lambda x: keras.layers.PReLU('prelu', x),
+                            use_bias=True):
+            l = keras.Sequential([
+                keras.layers.Conv2D('conv0', 32, 8, strides=4),
+                keras.layers.Conv2D('conv1', 64, 4, strides=2),
+                keras.layers.Conv2D('conv2', 64, 3),
+                keras.layers.Dense('fc0', 512),                     # FullyConnected
+                keras.layers.LeakyReLU(alpha=0.01),
+            ])
+        
+        if self.method != 'Duebling':
+            V = keras.layers.Dense('fct', l, 1)
+            AS = keras.layers.Dense('fctA', l, self.num_actions)
+            Q = tf.add(AS, V - self.reduce_mean(AS, l, keep_dims=True))
+        
+        return tf.identity(Q, name='QValue')
+
+
+def get_config(model):
+    global args 
+    expreplay = ExReplay(
+        predictor_io_names=(['state'], ['QValue']),
+        get_player=lambda: get_player(train=True),
+        num_parallel_players=NUM_PARALLEL_PLAYERS,
+        state_shape=model.state_shape,
+        batch_size=BATCH_SIZE,
+        memory_size=MEMORY_SIZE,
+        init_memory_size=INIT_MEMORY_SIZE,
+        update_frequency=UPDATE_FREQUENCE,
+        history_len=FRAME_HISTORY,
+        state_dtype=model.state_dtype.as_numpy_dtype
+    )
+
+    return keras.callbacks(
+        data=tf.data(expreplay),
+        model=model,
+        callback=[
+            keras.callbacks.ModelCheckpoint(),
+            keras.callbacks(
+                tf.function(DQNModel.update_target_param, verbose=True),
+                every_k_steps=5000
+            ),
+            expreplay,
+            keras.callbacks.LearningRateScheduler(
+                setattr(expreplay, 'exploration'),
+                [(0, 1), (10, 0.1), (400, 0.01)],       # 1 -> 0.1 in the first million steps
+                interp='linear'
+            ),
+            keras.callbacks(Evaluator(
+                args.num_eval, ['state'], ['QValue'], get_player),
+                every_k_epochs=5 if 'pong' in args.env.lower() else 10),    # eval more frequently for easy games
+        ],
+        steps_per_epoch=STEPS_PER_EPOCH,
+        max_epoch=500,      # a total of 50M state transition
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpu", help="comma separated list of GPU(s) to use.")
+    parser.add_argument("--load", help="load model")
+    parser.add_argument("--task", help="task to perform", choices=["play", "eval", "train"], default="train")
+    parser.add_argument("--env", required=True, help="either an atari rom file (that ends with .bin) or a gym atari environment name")
+    parser.add_argument("--algo", help='algorithm', choices=["DQN", "Double", "Dueling"], default="Double")
+    parser.add_argument("--num-eval", default=50, type=int)
+    
+    args = parser.parse_args()
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    ENV_NAME = args.env
+    USE_GYM = not ENV_NAME.endswith('.bin')
+
+    # set num_actions
+    num_actions = get_player().action_space.n
+    logger.info(f"ENV: {args.env}, Num Actions: {num_actions}")
+
+    state_shape = IMAGE_SIZE + (3, ) if USE_GYM else IMAGE_SIZE
+    model = Model(state_shape, FRAME_HISTORY, args.algo, num_actions)
+
+    if args.task != "train":
+        assert args.load is not None
+
+        pred = model.predict(keras.Model.predict(
+            model=model,
+            session_init=keras.initializers.GlorotUniform(args.load),
+            input_names=['state'],
+            output_names=['QValue'],
+        ))
+    
+    if args.task == "play":
+        play_n_episodes(get_player(viz=0.01), pred, 100, render=True)
+    elif args.task == "eval":
+        eval_model_multithread(pred, args.num_eval, get_player)
+    else:
+        logger.set_logger_dir(
+            os.path.join("train_log", f"DQN-{os.path.basename(args.env).split('.'[0])}")
+        )
+        config = get_config(model)
+        config.session_init = keras.initializers.GlorotUniform(args.load)
+        model.fit()(config, model.fit())
